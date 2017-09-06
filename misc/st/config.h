0c6af66fadcf990d93092214e09a1f668503ce4f
Awal Garg <awalgarg@gmail.com>
2017-09-06 18:05:59 +0530

    revert to st's default TERM

--- misc/st/config.h
+++ misc/st/config.h
@@ -63,7 +63,7 @@ unsigned int cursorthickness = 1;
 static int bellvolume = 0;
 
 /* default TERM value */
-char termname[] = "xterm-256color";
+char termname[] = "st-256color";
 
 /*
  * spaces per tab

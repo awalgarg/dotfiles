34f1225e4b89155dc46655741bdd96b1b2b00b4e
Awal Garg <awalgarg@gmail.com>
2018-01-07 23:28:08 +0530

    plain characters for git status

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,10 +12,10 @@ set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
 
 # Status Chars
-set __fish_git_prompt_char_dirtystate '⚡'
-set __fish_git_prompt_char_stagedstate '→'
-set __fish_git_prompt_char_untrackedfiles '☡'
-set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_dirtystate '/'
+set __fish_git_prompt_char_stagedstate '>'
+set __fish_git_prompt_char_untrackedfiles 'u'
+set __fish_git_prompt_char_stashstate 's'
 set __fish_git_prompt_char_upstream_ahead '+'
 set __fish_git_prompt_char_upstream_behind '-'
 

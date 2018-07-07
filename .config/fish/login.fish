956970d5ec4dd19b11a83b0ee9f5ccb5ac89a283
Awal Garg <awalgarg@gmail.com>
2018-07-07 07:05:38 +0530

    use find instead of shell expansion for adding ssh keys

--- .config/fish/login.fish
+++ .config/fish/login.fish
@@ -6,7 +6,7 @@ if which ssh-agent > /dev/null ^ /dev/null
 	if not pgrep -u $USER ssh-agent > /dev/null ^ /dev/null
 		ssh-agent -c > ~/.ssh-agent-details
 		eval (cat ~/.ssh-agent-details) > /dev/null ^ /dev/null
-		ssh-add ~/.ssh/*rsa > /dev/null ^ /dev/null
+		find ~/.ssh/ -name '*rsa' -exec ssh-add '{}' \; > /dev/null ^ /dev/null
 	else
 		eval (cat ~/.ssh-agent-details) > /dev/null ^ /dev/null
 	end

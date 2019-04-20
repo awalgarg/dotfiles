282d3e14511fad1ea44e61ba8c139d9dbe7e658b
Awal Garg <awalgarg@gmail.com>
2019-04-21 01:17:04 +0530

    fish: remove ssh-agent initialization in favour of IdentityFile rules
    This method rids us the trouble of having to write the same code for
    different shells for when switching between login shells.

--- .config/fish/login.fish
+++ .config/fish/login.fish
@@ -1,14 +1,3 @@
 set PATH $HOME/bin $PATH
 set -x LANG 'en_US.UTF-8'
 set -x EDITOR vim
-
-if which ssh-agent > /dev/null ^ /dev/null
-	if not pgrep -u $USER ssh-agent > /dev/null ^ /dev/null
-		ssh-agent -c > ~/.ssh-agent-details
-		eval (cat ~/.ssh-agent-details) > /dev/null ^ /dev/null
-		find ~/.ssh/ -name '*rsa' -exec ssh-add '{}' \; > /dev/null ^ /dev/null
-	else
-		eval (cat ~/.ssh-agent-details) > /dev/null ^ /dev/null
-	end
-end
-
